87fc7635d690eea84e27a982f96e3693075eb858
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-02-14 21:40:07 +0530

    Create Cookie.java

---
+++ Cookie.java
@@ -0,0 +1,21 @@
+package misc.classes;
+
+public class Cookie 
+{
+	private String color;
+	public Cookie(String Color)
+	{
+		this.color = Color;
+	}
+	
+	public String getColor()
+	{
+		return color;
+	}
+	
+	public void setColor(String Color)
+	{
+		this.color = Color;
+	}
+}
+
