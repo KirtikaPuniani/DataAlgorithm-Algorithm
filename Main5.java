972f9d7e5710977044a322341103490c9349f5d2
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-02-26 02:08:11 +0530

    Create Main5.java

---
+++ Main5.java
@@ -0,0 +1,14 @@
+package datastructures.LinkedList;
+
+public class Main5
+{
+	public static void main(String[] args)
+	{
+		LinkedList6 myLinkedList = new LinkedList6(2);
+		myLinkedList.append(3);
+		
+		myLinkedList.prepend(1);
+		
+		myLinkedList.printList();
+	}
+}
