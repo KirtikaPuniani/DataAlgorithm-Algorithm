3bb42f71e9243aff21275a35aa1113723d1746b3
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-02-26 00:04:56 +0530

    Create Main4.java

---
+++ Main4.java
@@ -0,0 +1,15 @@
+package datastructures.LinkedList;
+
+public class Main4 
+{
+	public static void main(String[] args)
+	{
+		LinkedList5 myLinkedList = new LinkedList5(1);
+		myLinkedList.append(2);
+		
+		//myLinkedList.printList();
+		System.out.println(myLinkedList.removeLast()); //returns 2 nodes
+		System.out.println(myLinkedList.removeLast()); //returns 1 nodes
+		System.out.println(myLinkedList.removeLast()); //returns 0 nodes
+	}
+}
